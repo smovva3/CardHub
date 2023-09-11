e591450d6e7ba8af9456a7ccd8333942fe945267
smovva3 <movva.sanjana@gmail.com>
2023-09-11 12:19:18 -0500

    Initial Commit

---
+++ Exercise3_Movva_Sanjana/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Exercise3_Movva_Sanjana
+//
+//  Created by Sanjana Movva on 9/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exercise3_Movva_Sanjana/Exercise3_Movva_SanjanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise3_Movva_SanjanaApp.swift
+//  Exercise3_Movva_Sanjana
+//
+//  Created by Sanjana Movva on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Exercise3_Movva_SanjanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
